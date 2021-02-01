e965908dceb2a0c796daa830a78e8855df77ad7b
Namit Gupta <namigupt@adobe.com>
2021-02-01 11:22:55 +0530

    demo commit

---
+++ core/src/main/java/com/adobe/aemaacs/internal/search/services/Constants.java
@@ -0,0 +1,13 @@
+package com.adobe.aemaacs.internal.search.services;
+
+public abstract class Constants {
+
+	private Constants() {
+		
+	}
+	public static final String DATE_RANGE_LOWER_BOUND = "daterange.lowerBound";
+	public static final String DATE_RANGE_PROPERTY = "daterange.property";
+	public static final String DATE_RANGE_LOWER_OPERATION = "daterange.lowerOperation";
+	public static final String DATE_RANGE_UPPER_OPERATION= "daterange.upperOperation";
+	public static final String DATE_RANGE_UPPER_BOUND ="daterange.upperBound";
+}
