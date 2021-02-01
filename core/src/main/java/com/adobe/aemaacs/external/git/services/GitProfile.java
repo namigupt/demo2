96ed6cb41f459817f1c23a7af9166420b9bd917b
Namit Gupta <namigupt@adobe.com>
2021-02-01 18:19:30 +0530

    demo commit

---
+++ core/src/main/java/com/adobe/aemaacs/external/git/services/GitProfile.java
@@ -0,0 +1,82 @@
+package com.adobe.aemaacs.external.git.services;
+
+public class GitProfile {
+
+	private String userName;
+	private String password;
+	private String repository;
+
+	public GitProfile(String userName, String password, String repository) {
+		super();
+		this.userName = userName;
+		this.password = password;
+		this.repository = repository;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getRepository() {
+		return repository;
+	}
+
+	public void setRepository(String repository) {
+		this.repository = repository;
+	}
+
+	@Override
+	public String toString() {
+		return "GitProfile [userName=" + userName + ", password=" + password + ", repository=" + repository + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((password == null) ? 0 : password.hashCode());
+		result = prime * result + ((repository == null) ? 0 : repository.hashCode());
+		result = prime * result + ((userName == null) ? 0 : userName.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		GitProfile other = (GitProfile) obj;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
+		if (repository == null) {
+			if (other.repository != null)
+				return false;
+		} else if (!repository.equals(other.repository))
+			return false;
+		if (userName == null) {
+			if (other.userName != null)
+				return false;
+		} else if (!userName.equals(other.userName))
+			return false;
+		return true;
+	}
+
+}
