c419064f0d90b03f04463d492a8c47d05a8550d2
Namit Gupta <namigupt@adobe.com>
2021-02-04 10:52:06 +0530

    demo commit

---
+++ core/src/main/java/com/adobe/aemaacs/external/ui/ConfigurationList.java
@@ -0,0 +1,25 @@
+
+package com.adobe.aemaacs.external.ui;
+import java.util.List;
+
+import org.osgi.annotation.versioning.ConsumerType;
+
+import com.drew.lang.annotations.NotNull;
+
+/**
+ * Defines the {@code CloudConfigurationList} Sling Model used for the cloudconfig component.
+ *
+ */
+@ConsumerType
+public interface ConfigurationList {
+
+  /**
+   * Retrieve the list of CloudConfigurations for the specified request.
+   *
+   * @return the list of {@code CloudConfiguration}s
+   */
+  @NotNull
+  default List<Configuration> getCloudConfigurations() {
+      throw new UnsupportedOperationException();
+  }
+}
